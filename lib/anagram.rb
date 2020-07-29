f8f00e58a117a5159b8205a8b455aeaceda5b674
Sean Ransonette <sransonette@gmail.com>
2020-07-29 00:47:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,14 @@
 class Anagram 
   attr_accessor :anagram 
   
+  @@array = []
+  
   def initialize(anagram)
     @anagram = anagram
   end
   
   def match(string)
-    new_array = [] 
+    #new_array = [] 
     string.each do |a, b|
     
     #string.sort {|a, b| a <=> b}
